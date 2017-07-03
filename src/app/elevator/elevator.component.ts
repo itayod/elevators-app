45f76fc8f4e5e5e8d9daab4dadb67d7aaf19784d
Your Name <you@example.com>
2017-07-03 21:00:33 +0300

    add some basic style

--- src/app/elevator/elevator.component.ts
+++ src/app/elevator/elevator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'elevator',
@@ -6,12 +6,28 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./elevator.component.scss']
 })
 export class ElevatorComponent implements OnInit {
+
   @Input() floor: number;
+  @Input() id: number;
+  @Input() elementId: string;
+  element: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.elementId = 'elevator-' + this.id;
+  }
 
+  ngAfterViewInit() {
+    this.element = document.getElementById(this.elementId);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['floor']) {
+      if(this.element) {
+        this.element.style.top = this.floor * 20 + 'px';
+      }
+    }
   }
 
 }
